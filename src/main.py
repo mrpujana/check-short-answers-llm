a01f594e50213a743047daac04168b7288ca2828
mrpujana <mr.pujana@gmail.com>
2024-04-22 20:46:37 +0545

    first commit

--- src/main.py
+++ src/main.py
@@ -9,13 +9,15 @@ GOOGLE_API_KEY = config["GOOGLE_API_KEY"]
 st.write("# Checker")
 
 checker = AnswerChecker(GOOGLE_API_KEY)
+
 question = "Define polymorphism in OOP."
+
 full_marks = st.selectbox("Marks: ", range(1, 11))
+
 answer = st.text_input(question, placeholder="Type your answer here")
+
+
 feedback = checker.generate_feedback(question, answer, full_marks)
-# feedback_=json.loads(feedback)
-print("x"*25)
-print(feedback)
 feedback_cleaning = feedback.strip('```json').strip('```')
 feedback=json.loads(feedback_cleaning)
 st.write(feedback['marks'])
